c7e82363267005a02ed6c0f00d74af54703c0d3f
Andrew Van Slaars <avanslaars@gmail.com>
2017-05-28 12:34:11 -0400

    Lesson 1 - bootstrap with yarn and CRA

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React with Redux</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
