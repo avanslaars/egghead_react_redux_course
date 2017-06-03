896dd2fa72b233da33911027e9db8d443e6129ea
Andrew Van Slaars <avanslaars@gmail.com>
2017-06-03 16:21:28 -0400

    Refactor to bindActionCreators

--- src/index.js
+++ src/index.js
@@ -4,16 +4,16 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 import store from './store'
+import {bindActionCreators} from 'redux'
 import {updateCurrent} from './reducers/todo'
 
-const todoChangeHandler = (val) =>
-  store.dispatch(updateCurrent(val))
+const actions = bindActionCreators({updateCurrent}, store.dispatch)
 
 const render = () => {
   const state = store.getState()
   ReactDOM.render(<App todos={state.todos}
     currentTodo={state.currentTodo}
-    changeCurrent={todoChangeHandler}
+    changeCurrent={actions.updateCurrent}
      />,
   document.getElementById('root'));
 }
