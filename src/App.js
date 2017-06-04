cde6900652a18d3b0f5f82334d4bf9bfd8717566
Andrew Van Slaars <avanslaars@gmail.com>
2017-06-04 10:31:52 -0400

    mapDispatchToProps shorthand and refactoring

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import logo from './logo.svg';
 import './App.css';
 import TodoForm from './components/TodoForm'
 import TodoList from './components/TodoList'
-import {bindActionCreators} from 'redux'
 import {updateCurrent} from './reducers/todo'
 
 class App extends Component {
@@ -27,7 +26,7 @@ class App extends Component {
   }
 }
 
-const mapStateToProps = (state) => state
-const mapDispatchToProps = (dispatch) => bindActionCreators({updateCurrent}, dispatch)
-const ConnectedApp = connect(mapStateToProps, mapDispatchToProps)(App)
-export default ConnectedApp
+export default connect(
+  (state) => state,
+  {updateCurrent}
+)(App)
